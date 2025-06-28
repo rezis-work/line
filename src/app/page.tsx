d9b466e127f08105c5821e2339936068ce758746
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-06-28 15:48:33 +0400

    implement inggest background job with AI _ with achiving anything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -16,11 +19,12 @@ export default function Home() {
   );
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
         onClick={() =>
           invoke.mutate({
-            text: "Testinio",
+            value: value,
           })
         }
       >
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "summarizer",
+      system:
+        "You are an expert Next js developer.  You write readable, concise, maintainable code. you write simple Next.js & React.js snippents like button component, input component, etc.",
+      model: openai({ model: "gpt-3.5-turbo" }),
+    });
+
+    const { output } = await codeAgent.run(`
+       Write the folowing snippent: ${event.data.value}
+      `);
+
+    return { output };
   }
 );
