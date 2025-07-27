5bfe4af7de11647a71c750cb873b36939bb71001
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-07-28 00:50:00 +0400

    implement sandbox

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,4 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "line" });
+export const inngest = new Inngest({ id: "line-ai" });
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,17 @@
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("line-nextjs-production");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "summarizer",
       system:
@@ -17,6 +23,12 @@ export const helloWorld = inngest.createFunction(
        Write the folowing snippent: ${event.data.value}
       `);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
